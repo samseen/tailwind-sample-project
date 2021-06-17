139d4f6bc589e8aa0179fd9e6dcfec996af8c79a
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-17 14:46:58 +0100

    Added the content for the menu page.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer'
 import Home from './pages';
 import About from './pages/about';
+import Menu from './pages/menu';
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Switch>
         <Route path="/" exact component={Home} />
+        <Route path="/menu" exact component={Menu} />
         <Route path="/about" exact component={About} />
       </Switch>
       <Footer />
