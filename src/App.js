f0d20232fb9b38a68f7b6fbbf87f439ccbd1c96b
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-17 14:33:42 +0100

    Completed the footer and added other pages.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Content from './components/Content'
+import Content from './components/Content';
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Navbar />
       <Hero />
       <Content />
+      <Footer />
     </>
   );
 }
