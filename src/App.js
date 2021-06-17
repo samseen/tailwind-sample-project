fa87465c4a0194ebdb872fde4718f72696a7865e
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-17 13:51:55 +0100

    Created the content section.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
+import Content from './components/Content'
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Content />
     </>
   );
 }
