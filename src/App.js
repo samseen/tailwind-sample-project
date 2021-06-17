15958b3a9243831bafee20d4bd5615820637e719
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-17 14:51:33 +0100

    Created the dropdown menu component.

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Footer from './components/Footer'
 import Home from './pages';
 import About from './pages/about';
 import Menu from './pages/menu';
+import Dropdown from './components/Dropdown'
 import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <>
       <Navbar />
+      <Dropdown />
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/menu" exact component={Menu} />
