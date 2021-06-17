23869bc332cb8cf4956d9e1ee9a6dc7e92415d21
Samson Akanbi <samson.akanbi@outlook.com>
2021-06-17 13:21:22 +0100

    Created the navigation bar.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-red-500">Yoooo</h1>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
